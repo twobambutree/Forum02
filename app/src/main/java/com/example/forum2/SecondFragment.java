c08a1dcd7de6c46e656690107c9030c55b08faef
PadThai <patiphan@gmail.com>
2021-11-16 14:14:49 -0500

    add and update functions to get data for announcement

--- app/src/main/java/com/example/forum2/SecondFragment.java
+++ app/src/main/java/com/example/forum2/SecondFragment.java
@@ -1,10 +1,8 @@
 package com.example.forum2;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -14,7 +12,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -32,13 +29,13 @@ import com.google.firebase.database.ValueEventListener;
  */
 public class SecondFragment extends Fragment {
 
-    FirebaseDatabase firebaseDatabase;
+    FirebaseDatabase db;
 
-    DatabaseReference databaseReference;
+    DatabaseReference subData, discData;
 
-    private TextView retrieveTV;
+    TextView subjectLine, discriptionLine;
 
-    Button viewAnnounce;
+//    Button viewAnnounce;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -62,18 +59,19 @@ public class SecondFragment extends Fragment {
      * @return A new instance of fragment SecondFragment.
      */
     // TODO: Rename and change types and number of parameters
-//    public static SecondFragment newInstance(String param1, String param2) {
-//        SecondFragment fragment = new SecondFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
+    public static SecondFragment newInstance(String param1, String param2) {
+        SecondFragment fragment = new SecondFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fragment_second);
 //        if (getArguments() != null) {
 //            mParam1 = getArguments().getString(ARG_PARAM1);
 //            mParam2 = getArguments().getString(ARG_PARAM2);
@@ -81,25 +79,25 @@ public class SecondFragment extends Fragment {
 
         // below line is used to get the instance
         // of our Firebase database.
-        firebaseDatabase = FirebaseDatabase.getInstance();
+//        firebaseDatabase = FirebaseDatabase.getInstance();
 
         // below line is used to get
         // reference for our database.
-        databaseReference = firebaseDatabase.getReference("Announcement");
+//        databaseReference = firebaseDatabase.getReference("Announcement");
+//        databaseReference = firebaseDatabase.getReference("Data");
 
         // initializing our object class variable.
 //        retrieveTV = findViewById(R.id.idTVRetrieveData);
 
-        getdata();
-    }
+//        getdata();
+//    }
 
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        View view =  inflater.inflate(R.layout.fragment_second, container, false);
-//
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_second, container, false);
+
 //        viewAnnounce = view.findViewById(R.id.viewAnnouncements);
-//
 //        viewAnnounce.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -109,16 +107,33 @@ public class SecondFragment extends Fragment {
 //
 //            }
 //        });
-//
-//        return view;
-//    }
+
+        // below line is used to get the instance
+        // of our Firebase database.
+        db = FirebaseDatabase.getInstance();
+
+        // below line is used to get
+        // reference for our database.
+        subData = db.getReference("Announcement/annSubject");
+        discData = db.getReference("Announcement/annDetail");
+
+        // initializing our object class variable.
+        subjectLine = view.findViewById(R.id.subjectLine);
+        discriptionLine = view.findViewById(R.id.discriptionLine);
+
+        // calling method
+        // for getting data.
+        getdata();
+
+        return view;
+    }
 
 
     private void getdata() {
 
         // calling add value event listener method
         // for getting the values from database.
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        subData.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 // this method is call to get the realtime
@@ -131,15 +146,38 @@ public class SecondFragment extends Fragment {
 
                 // after getting the value we are setting
                 // our value to our text view in below line.
-                retrieveTV.setText(value);
-                System.out.println(retrieveTV);
+                subjectLine.setText(value);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 // calling on cancelled method when we receive
                 // any error or we are not able to get the data.
-//                Toast.makeText(MainActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
+                // Toast.makeText(MainActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        discData.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // this method is call to get the realtime
+                // updates in the data.
+                // this method is called when the data is
+                // changed in our Firebase console.
+                // below line is for getting the data from
+                // snapshot of our database.
+                String value = snapshot.getValue(String.class);
+
+                // after getting the value we are setting
+                // our value to our text view in below line.
+                discriptionLine.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // calling on cancelled method when we receive
+                // any error or we are not able to get the data.
+                // Toast.makeText(MainActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
             }
         });
     }
